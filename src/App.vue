e8cf41edb241a48356fba11afea71c67da7a393e
Noah Farjon <noahmansland@gmail.com>
2020-11-17 15:37:33 +0100

    image working

---
+++ src/App.vue
@@ -0,0 +1,114 @@
+<template>
+  <div id="app">
+    <b-row  class="mb-4">
+      <b-col class="ml-5">
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col>
+    <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col>
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col>
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col class="mr-5">
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+    </b-row>
+
+    <b-row  class="mb-4">
+      <b-col class="ml-5">
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col>
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col>
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col>
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col class="mr-5">
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+    </b-row>
+    <b-row  class="mb-4">
+      <b-col class="ml-5">
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col>
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col>
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col>
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col class="mr-5">
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+    </b-row>
+    <b-row  class="mb-4">
+      <b-col class="ml-5">
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col>
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col>
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col>
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col class="mr-5">
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+    </b-row>
+    <b-row  class="mb-4">
+      <b-col class="ml-5">
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col>
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col>
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col>
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+      <b-col class="mr-5">
+        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      </b-col>
+    </b-row>
+
+  </div>
+</template>
+
+<script>
+
+import numero from "@/components/numero";
+
+export default {
+  name: 'App',
+  components: {
+    numero
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/numero.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+
+  <b-img-lazy :src="imgsrc"></b-img-lazy>
+
+</div>
+</template>
+
+<script>
+export default {
+  props : {
+    imgsrc: String,
+
+  } ,
+  name: "Textimage"
+}
+</script>
+
+<style scoped>
+
+</style>
