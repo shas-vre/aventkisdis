cc5aae5bd3b6315d4bb2cbbfc57f2e2690d44f9c
Noah Farjon <noahmansland@gmail.com>
2020-11-18 14:47:10 +0100

    background image

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,94 @@
 <template>
-  <div id="app">
-    <b-row  class="mb-4">
-      <b-col class="ml-5">
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
+  <div class="bg">
+  <div class="container">
+    <b-row  class=" justify-content-md-center padding-0 no-gutters p-0 m-0">
+      <b-col class="padding-0">
+        <numero imgsrc="https://cdn.discordapp.com/attachments/535055164072263680/778569214340366336/roundcube.png" class="padding-0"></numero>
       </b-col>
-      <b-col>
-    <numero imgsrc="https://via.placeholder.com/125"></numero>
+      <b-col class="padding-0">
+    <numero imgsrc="https://cdn.discordapp.com/attachments/535055164072263680/778569214340366336/roundcube.png" class="padding-0"></numero>
       </b-col>
-      <b-col>
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      <b-col class="padding-0">
+        <numero imgsrc="https://cdn.discordapp.com/attachments/535055164072263680/778569214340366336/roundcube.png"></numero>
       </b-col>
-      <b-col>
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      <b-col class="padding-0">
+        <numero imgsrc="https://cdn.discordapp.com/attachments/535055164072263680/778569214340366336/roundcube.png"></numero>
       </b-col>
-      <b-col class="mr-5">
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-    </b-row>
-
-    <b-row  class="mb-4">
-      <b-col class="ml-5">
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-      <b-col>
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-      <b-col>
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-      <b-col>
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-      <b-col class="mr-5">
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-    </b-row>
-    <b-row  class="mb-4">
-      <b-col class="ml-5">
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-      <b-col>
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-      <b-col>
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-      <b-col>
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-      <b-col class="mr-5">
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-    </b-row>
-    <b-row  class="mb-4">
-      <b-col class="ml-5">
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-      <b-col>
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-      <b-col>
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-      <b-col>
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-      <b-col class="mr-5">
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-    </b-row>
-    <b-row  class="mb-4">
-      <b-col class="ml-5">
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-      <b-col>
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-      <b-col>
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-      <b-col>
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
-      </b-col>
-      <b-col class="mr-5">
-        <numero imgsrc="https://via.placeholder.com/125"></numero>
+      <b-col class="padding-0">
+        <numero imgsrc="https://cdn.discordapp.com/attachments/535055164072263680/778569214340366336/roundcube.png"></numero>
       </b-col>
     </b-row>
 
+    <b-row  class=" justify-content-md-center padding-0 no-gutters p-0 m-0">
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300" class="padding-0"></numero>
+      </b-col>
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300" class="padding-0"></numero>
+      </b-col>
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300"></numero>
+      </b-col>
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300"></numero>
+      </b-col>
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300"></numero>
+      </b-col>
+    </b-row>
+    <b-row  class=" justify-content-md-center padding-0 no-gutters p-0 m-0">
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300" class="padding-0"></numero>
+      </b-col>
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300" class="padding-0"></numero>
+      </b-col>
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300"></numero>
+      </b-col>
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300"></numero>
+      </b-col>
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300"></numero>
+      </b-col>
+    </b-row>
+    <b-row  class=" justify-content-md-center padding-0 no-gutters p-0 m-0">
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300" class="padding-0"></numero>
+      </b-col>
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300" class="padding-0"></numero>
+      </b-col>
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300"></numero>
+      </b-col>
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300"></numero>
+      </b-col>
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300"></numero>
+      </b-col>
+    </b-row>
+    <b-row  class=" justify-content-md-center padding-0 no-gutters p-0 m-0">
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300" class="padding-0"></numero>
+      </b-col>
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300" class="padding-0"></numero>
+      </b-col>
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300"></numero>
+      </b-col>
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300"></numero>
+      </b-col>
+      <b-col class="padding-0">
+        <numero imgsrc="https://via.placeholder.com/300"></numero>
+      </b-col>
+    </b-row>
+
+  </div>
   </div>
 </template>
 
@@ -103,6 +105,26 @@ export default {
 </script>
 
 <style>
+.bg {
+  /* The image used */
+  background-image: url("https://mdbootstrap.com/img/Photos/Horizontal/Nature/full page/img(20).jpg");
+
+  /* Full height */
+  height: 100%;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.padding-0{
+  padding-right:0;
+  padding-left:0;
+  padding-top: 0;
+  padding-bottom: 0;
+  margin-left: 0;
+  margin-right: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
